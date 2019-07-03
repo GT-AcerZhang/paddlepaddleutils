6e138656b18f2475f7b51a1dab3353b85b77fde0
gongweibao <gongweibao@baidu.com>
2019-07-03 15:42:48 +0800

    repair install

--- cloud_cuda9_cudnn7/build_push.sh
+++ cloud_cuda9_cudnn7/build_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rpc_image=registry.baidu.com/gongweibao/distributed_paddle_dgc:cud9_cudnn7
-docker build --network host . -t ${rpc_image}
+#docker build --no-cache  --network host . -t ${rpc_image}
+docker build  --network host . -t ${rpc_image}
 docker push ${rpc_image}
 
--- cloud_cuda9_cudnn7/prepare.sh
+++ cloud_cuda9_cudnn7/prepare.sh
@@ -4,7 +4,7 @@ mkdir -p whl
 rm -f whl/*.whl
 
 cp \
-/ssd2/gongwb/go/src/github.com/PaddlePaddle/Paddle/build/build_cent_chengduo16393_release_gpu_y_grpc/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl \
+/ssd2/gongwb/go/src/github.com/PaddlePaddle/Paddle/build/build_cent_fuse_fp16_all_reduce_release_gpu_y_grpc/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl \
 ./whl/
 
 
