6a78901c9392ea4b585f6574aeabf9641b49313a
gongweibao <weibao.gong@gmail.com>
2020-08-27 06:51:25 +0000

    test

--- build/start.sh
+++ build/start.sh
@@ -15,17 +15,17 @@ if grep --quiet "Ubuntu" /etc/issue; then
 elif grep --quiet "CentOS" /etc/issue; then
   os=cent
 
-  export LD_LIBRARY_PATH=/opt/_internal/cpython-2.7.11-ucs4/lib:${LD_LIBRARY_PATH#/opt/_internal/cpython-2.7.11-ucs2/lib:}
-  export PATH=/opt/python/cp27-cp27mu/bin/:${PATH}
-  PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp27-cp27mu/bin/python" 
+  #export LD_LIBRARY_PATH=/opt/_internal/cpython-2.7.11-ucs4/lib:${LD_LIBRARY_PATH#/opt/_internal/cpython-2.7.11-ucs2/lib:}
+  #export PATH=/opt/python/cp27-cp27mu/bin/:${PATH}
+  #PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp27-cp27mu/bin/python" 
   export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
 else
   echo "can't find os, exit"
   exit 1
 fi
 
-arr=("v1.8.2.1" "v1.7.1.1" "v1.6.3.1" "v1.6.1.1")
-#arr=("v1.7.1.1" "v1.6.3.1" "v1.6.1.1")
+#arr=("v1.8.2.1" "v1.7.1.1" "v1.6.3.1" "v1.6.1.1")
+arr=("v1.7.1.1" "v1.6.3.1" "v1.6.1.1")
 #arr=("v1.6.3.1" "v1.6.1.1")
 #arr=("v1.6.1.1")
 for v in ${arr[@]} ; do
@@ -35,23 +35,25 @@ for v in ${arr[@]} ; do
     echo "processing:"$v 
     git rev-parse HEAD
 
-    #rm -rf third_party/${os}_${v}_release_gpu_2.7/inference_demo/
+    for py_v in "2.7" "3.5.1" "3.6.0" "3.7.0" ; do
+        #rm -rf third_party/${os}_${v}_release_gpu_${py_v}/inference_demo/
 
-    slim_path=build_${os}_${v}_release_gpu_y_grpc_2.7/python/paddle/fluid/contrib/slim/tests
-    rm -rf ${slim_path}/checkpoints_uniform_restore/ ${slim_path}/checkpoints_uniform_restore_tmp/
+        slim_path=build_${os}_${v}_release_gpu_y_grpc_${py_v}/python/paddle/fluid/contrib/slim/tests
+        rm -rf ${slim_path}/checkpoints_uniform_restore/ ${slim_path}/checkpoints_uniform_restore_tmp/
 
-    #rm -rf build_${os}_${v}_release_gpu_y_grpc_2.7
+        #rm -rf build_${os}_${v}_release_gpu_y_grpc_${py_v}
 
-    paddle_version=`echo ${v} |  cut -c 2-6`
-    export PADDLE_VERSION=${paddle_version}
+        paddle_version=`echo ${v} |  cut -c 2-6`
+        export PADDLE_VERSION=${paddle_version}
 
-    ./build.sh -py_v 3.5.1
-    ./build.sh -py_v 3.6.0
-    ./build.sh -py_v 3.7.0
+        ./build.sh -py_v $py_v
 
-    #pushd build_${os}_${v}_release_gpu_y_grpc_2.7
-    #set +e
-    #../paddle_build.sh test
-    #set -e
-    #popd
+        #pushd build_${os}_${v}_release_gpu_y_grpc_2.7
+        #set +e
+        #../paddle_build.sh test
+        #set -e
+        #popd
+
+    done
+
 done
