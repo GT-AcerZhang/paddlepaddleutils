f22a0ae24339c657e8231a2909ce05df39eb2af5
gongweibao <weibao.gong@gmail.com>
2020-11-10 13:50:22 +0000

    add

--- build/build_push.sh
+++ build/build_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-image=registry.baidu.com/gongweibao/paddle-dev:cuda10.0-cudnn7
+image=registry.baidu.com/gongweibao/paddle-dev:cuda10.2-cudnn7-gcc82
 
 docker build --pull --network host --build-arg UBUNTU_MIRROR=http://ftp.sjtu.edu.cn/ubuntu/  . -t ${image}
 #docker build  --network host . -t ${image}
--- patch/patch.sh
+++ patch/patch.sh
@@ -1,6 +1,8 @@
 set -e
 #path="https://raw.githubusercontent.com/PaddlePaddle/Paddle/develop"
-path="https://raw.githubusercontent.com/gongweibao/Paddle/autorestorediff"
+#path="https://raw.githubusercontent.com/gongweibao/Paddle/autorestorediff"
+#path="https://raw.githubusercontent.com/PaddlePaddle/Paddle/release/1.8"
+path="https://raw.githubusercontent.com/gongweibao/Paddle/v1.8.2.1"
 #shell.cc
 wget ${path}/paddle/fluid/framework/io/shell.cc -O paddle/fluid/framework/io/shell.cc
 wget ${path}/paddle/fluid/framework/io/shell.h -O paddle/fluid/framework/io/shell.h
@@ -15,6 +17,7 @@ if [[ -f python/paddle/fluid/incubate/fleet/utils/hdfs.py ]]; then
     git rm python/paddle/fluid/incubate/fleet/utils/hdfs.py
 fi
 
+
 # checkpoint and unittest
 mkdir -p python/paddle/fluid/incubate/checkpoint
 touch python/paddle/fluid/incubate/checkpoint/__init__.py 
@@ -24,17 +27,25 @@ wget ${path}/python/paddle/fluid/tests/unittests/test_fleet_checkpoint.py -O pyt
 wget ${path}/python/paddle/fluid/tests/unittests/test_auto_checkpoint.py -O python/paddle/fluid/tests/unittests/test_auto_checkpoint.py
 wget ${path}/python/paddle/fluid/tests/unittests/test_auto_checkpoint2.py -O python/paddle/fluid/tests/unittests/test_auto_checkpoint2.py
 
-#gwb_path="https://raw.githubusercontent.com/gongweibao/Paddle/1483b31a9a200cd819c73cacf65399cac8b93307"
-gwb_path="https://raw.githubusercontent.com/gongweibao/Paddle/autorestorediff"
-wget ${gwb_path}/python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint.py -O python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint.py
-wget ${gwb_path}/python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint2.py -O python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint2.py
-wget ${gwb_path}/python/paddle/fluid/incubate/checkpoint/dataloader_auto_checkpoint.py -O python/paddle/fluid/incubate/checkpoint/dataloader_auto_checkpoint.py
-wget ${gwb_path}/python/paddle/fluid/tests/unittests/auto_checkpoint_utils.py -O python/paddle/fluid/tests/unittests/auto_checkpoint_utils.py
-
 # hdfs
 wget ${path}/python/paddle/fluid/tests/unittests/test_fs_interface.py -O python/paddle/fluid/tests/unittests/test_fs_interface.py
 wget ${path}/python/paddle/fluid/tests/unittests/test_hdfs.py -O python/paddle/fluid/tests/unittests/test_hdfs.py
 mkdir -p python/paddle/fluid/incubate/fleet/utils
 touch python/paddle/fluid/incubate/fleet/utils/__init_.py
-wget ${path}/python/paddle/fleet/utils/fs.py -O ./python/paddle/fluid/incubate/fleet/utils/fs.py
+
+#path="https://raw.githubusercontent.com/gongweibao/Paddle/v1.8.2.1"
+wget ${path}/python/paddle/fluid/incubate/fleet/collective/__init__.py -O python/paddle/fluid/incubate/fleet/collective/__init__.py
+wget ${path}/python/paddle/fluid/incubate/fleet/utils/fleet_util.py -O python/paddle/fluid/incubate/fleet/utils/fleet_util.py
+wget ${path}/python/paddle/fluid/incubate/fleet/utils/fs.py -O python/paddle/fluid/incubate/fleet/utils/fs.py
+wget ${path}/python/paddle/fluid/tests/unittests/test_checkpoint_saver.py -O python/paddle/fluid/tests/unittests/test_checkpoint_saver.py
+
+# diff
+#gwb_path="https://raw.githubusercontent.com/gongweibao/Paddle/autorestorediff"
+gwb_path=$path
+wget ${gwb_path}/python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint.py -O python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint.py
+wget ${gwb_path}/python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint2.py -O python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint2.py
+wget ${gwb_path}/python/paddle/fluid/incubate/checkpoint/dataloader_auto_checkpoint.py -O python/paddle/fluid/incubate/checkpoint/dataloader_auto_checkpoint.py
+wget ${gwb_path}/python/paddle/fluid/tests/unittests/auto_checkpoint_utils.py -O python/paddle/fluid/tests/unittests/auto_checkpoint_utils.py
+#wget ${path}/python/paddle/fleet/utils/fs.py -O ./python/paddle/fluid/incubate/fleet/utils/fs.py
+
 echo "complete"
