64ebe41693d2f82c4f1261ab8b26a84b06791a28
hutuxian <hutuxian@sina.cn>
2021-02-09 16:08:30 +0800

    add

--- ascend/insert.sh
+++ ascend/insert.sh
@@ -8,14 +8,17 @@ awk 'BEGIN {
          for (i=1; i<=NF; i++)
              if ($i == "{") {
                  d++ ;
-                 if (d == 1) printf "{\n"
+                 if (d == 1) {
+                     printf "{\n"
+                     printf "\t[__FUNCTION__, __LINE__]"
+                 }
              } else
              if ($i == "}") {
                  d-- ;
                  if (d == 0) printf "}"
              } else
-             if (d == 0)
-                 print("__line_no__")
+             #if (d == 0)
              printf "%s", $i ;
-         if (d == 0) printf "\n"
+         #if (d == 0) printf "\n"
+         printf "\n"
      }' ./test_init.cc
--- ascend/test_init.cc
+++ ascend/test_init.cc
@@ -8,29 +8,43 @@
 #include "graph/tensor.h"
 #include "graph/types.h"
 
-std::map<std::string, std::string> GetDefaultInitOptions() {
-	std::map<std::string, std::string> init_options;
-	init_options["ge.exec.deviceId"] = std::to_string(0);
-	init_options["ge.graphRunMode"] = std::to_string(1);
-	return init_options;
+std::shared_ptr<ge::Session> session_;
+
+std::map<ge::AscendString, ge::AscendString> _GetDefaultInitOptions() {
+    ge::AscendString prof_opts=R"({"output":"/tmp/profiling","training_trace":"on","fp_point":"resnet_model/conv2d/Conv2Dresnet_model/batch_normalization/FusedBatchNormV3_Reduce","bp_point":"gradients/AddN_70"})";
+
+	std::map<ge::AscendString, ge::AscendString> ge_options ;
+    ge_options["ge.exec.deviceId"] = "0";
+    ge_options["ge.graphRunMode"] = "1";
+    ge_options["ge.exec.profilingMode"] = "1";
+    ge_options["ge.exec.profilingOptions"] = prof_opts;
+
+	return ge_options;
 }
 
-std::map<std::string, std::string> GetDefaultInitSessionOptions() {
-	std::map<std::string, std::string> init_options;
-	init_options["a"] = "b";
-	init_options["ge.trainFlag"] = "1";
+std::map<ge::AscendString, ge::AscendString> _GetDefaultInitSessionOptions() {
+	std::map<ge::AscendString, ge::AscendString> init_options;
 	return init_options;
 }
 
 void InitGlobalResouces() {
 	std::cout << "Begin InitGlobalResouces";
-	ge::Status status = ge::GEInitialize(GetDefaultInitOptions());
+	ge::Status status = ge::GEInitialize(_GetDefaultInitOptions());
 
-	auto ss = new ge::Session(GetDefaultInitSessionOptions());
-	std::cout << "End InitGlobalResouces" << status << &ss;
+    session_.reset(new ge::Session(_GetDefaultInitSessionOptions()));
+	std::cout << "End InitGlobalResouces" << status << session_;
 }
+
+void DestroyGlobalResources() {
+    std::cout << "Begin ascend DestroyGlobalResouces";
+    session_ = nullptr;
+    ge::GEFinalize();
+    std::cout << "Begin ascend DestroyGlobalResouces";
+}
+
 
 int main(){
 	InitGlobalResouces();	
+    DestroyGlobalResources();
 	return 0;
 }
